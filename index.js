ce7b6ff86bcafcbaadd9527b9037d3e6f8392c43
Guan Bo <guanbo2002@gmail.com>
2017-10-10 00:29:25 +0800

    Add: upload file

--- index.js
+++ index.js
@@ -2,19 +2,12 @@
 
 const superagent = require('superagent');
 const prefix = require('superagent-prefix')('http://0.0.0.0:3000');
-const app = require('../../server/server');
+const fs = require('fs');
+const path = require('path');
+const formstream = require('formstream');
 
 let server, accessToken;
 
-before((done) => {
-  server = app.start();
-  app.on('started', done);
-});
-
-after((done) => {
-  server.close();
-  done();
-});
 
 let request = {
   credential: {
@@ -49,12 +42,28 @@ let request = {
     if (accessToken) req.set({ 'Authorization': accessToken.id });
     req.send(json).end(done);
   },
+  upload: (route, filepath, done) => {
+    var req = superagent.post(route).use(prefix).accept('*/*');
+    if (accessToken) req.set({ 'authorization': accessToken.id });
+
+    req.on('response', function (res) {
+      done(null, res);
+    });
+
+    fs.stat(filepath, function (err, stat) {
+      if (err) return done(err);
+      var form = formstream();
+      form.file('buffer', filepath, path.basename(filepath), stat.size);
+      req.set(form.headers());
+      form.pipe(req);
+    });
+  },
   login: (credential, done) => {
     if (typeof credential === 'function') {
       done = credential;
       credential = null;
     }
-    credential = credential||request.credential;
+    credential = credential || request.credential;
     request.post('/api/users/login?include=user', credential, (err, res) => {
       accessToken = res.body;
       done(err);
@@ -70,9 +79,20 @@ let request = {
   },
 };
 
+module.exports.boot = (app) => {
+  before((done) => {
+    server = app.start();
+    app.on('started', done);
+  });
+  
+  after((done) => {
+    server.close();
+    done();
+  });
+}
 module.exports.accessToken = () => { return accessToken; };
-module.exports.request = (options) => { 
-  if (!options||!options.credential) {
+module.exports.request = (options) => {
+  if (!options || !options.credential) {
     throw new Error('credential must provider!');
   }
   return Object.assign(request, options);
