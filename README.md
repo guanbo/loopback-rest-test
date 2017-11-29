5630d0fa6bb34cc42d1f6917ffae4afead375f11
Guan Bo <guanbo2002@gmail.com>
2017-11-29 12:55:07 +0800

    Bump 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # rest-test-bootstrap
 restful api test bootstrap
+
+## Quick Start
+
+```js
+const rest = require('loopback-rest-test');
+rest.boot(require('../../server/server'));
+
+describe('Find orders', ()=>{
+  const request = new rest.Request({
+    credential: { username: '18199999999', password: '123456' },
+    userModel: 'users',
+  });
+
+  before((done) => { request.login(done); });
+  after((done) => { request.logout(done); });
+
+  it('should ok', (done)=>{
+    request.get('/api/orders', {
+      filter: {
+        where: {title: 'abc'},
+        include: ['operator']
+      }
+    }, (err, res)=>{
+      res.should.have.status(200);
+      done();
+    });
+  });
+});
+
+
+```
--- index.js
+++ index.js
@@ -6,45 +6,58 @@ const fs = require('fs');
 const path = require('path');
 const formstream = require('formstream');
 
-let server, accessToken;
+let server;
 
+class Request {
+  constructor(options) {
+    this.credential = options.credential || {
+      username: 'weixin.oV5Yms7LX0RPudwkz1X6G2Kilj9w',
+      password: '123456',
+    };
+    this.userModel = options.userModel || 'users';
+    this.tokenKey = options.tokenKey || 'id';
+    this.accessToken = null;
+  }
 
-let request = {
-  credential: {
-    username: 'weixin.oV5Yms7LX0RPudwkz1X6G2Kilj9w',
-    password: '123456',
-  },
-  get: (route, query, done) => {
+  setAuthorization(req) {
+    let token = this.accessToken && this.accessToken[this.tokenKey];
+    if (token) req.set({ 'Authorization': token });
+  }
+
+  get(route, query, done) {
     if (typeof query === 'function') {
       done = query;
       query = null;
     }
     let req = superagent.get(route).use(prefix);
-    if (accessToken) req.set({ 'Authorization': accessToken.id });
+    this.setAuthorization(req);
     if (query) req.query(query);
     req.end(done);
-  },
-  post: (route, json, done) => {
+  }
+
+  post(route, json, done) {
     if (typeof json === 'function') {
       done = json;
       json = null;
     }
     let req = superagent.post(route).use(prefix);
-    if (accessToken) req.set({ 'Authorization': accessToken.id });
+    this.setAuthorization(req);
     req.send(json).end(done);
-  },
-  put: (route, json, done) => {
+  }
+
+  put(route, json, done) {
     if (typeof json === 'function') {
       done = json;
       json = null;
     }
     let req = superagent.put(route).use(prefix);
-    if (accessToken) req.set({ 'Authorization': accessToken.id });
+    this.setAuthorization(req);
     req.send(json).end(done);
-  },
-  upload: (route, filepath, done) => {
+  }
+
+  upload(route, filepath, done) {
     var req = superagent.post(route).use(prefix).accept('*/*');
-    if (accessToken) req.set({ 'authorization': accessToken.id });
+    this.setAuthorization(req);
 
     req.on('response', function (res) {
       done(null, res);
@@ -57,43 +70,41 @@ let request = {
       req.set(form.headers());
       form.pipe(req);
     });
-  },
-  login: (credential, done) => {
+  }
+
+  login(credential, done) {
     if (typeof credential === 'function') {
       done = credential;
       credential = null;
     }
-    credential = credential || request.credential;
-    request.post('/api/users/login?include=user', credential, (err, res) => {
-      accessToken = res.body;
+    credential = credential || this.credential;
+    let that = this;
+    this.post('/api/' + this.userModel + '/login?include=user', credential, (err, res) => {
+      that.accessToken = res.body;
       done(err);
     });
-  },
-  logout: (done) => {
-    if (accessToken) {
-      request.post('/api/users/logout', done);
-      accessToken = null;
-    } else {
-      done();
+  }
+
+  logout(done) {
+    if (this.accessToken) {
+      this.post('/api/' + this.userModel + '/logout');
     }
-  },
-};
+    this.accessToken = null;
+    done();
+  }
+
+}
 
 module.exports.boot = (app) => {
   before((done) => {
     server = app.start();
     app.on('started', done);
   });
-  
-  after((done) => {
-    server.close();
-    done();
+
+  after(() => {
+    server.close(()=> {
+      process.exit(0);
+    });
   });
 }
-module.exports.accessToken = () => { return accessToken; };
-module.exports.request = (options) => {
-  if (!options || !options.credential) {
-    throw new Error('credential must provider!');
-  }
-  return Object.assign(request, options);
-}
+module.exports.Request = Request;
